52bcd06e2ac495b6bdbb28d6d399b3ed53dc64f6
Shubham kumar <97464092+ShubhamWP07@users.noreply.github.com>
2023-06-25 13:21:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  // Add an element to the back of the queue
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  // Remove and return the front element of the queue
+  dequeue() {
+    if (this.isEmpty()) {
+      return "Queue Underflow";
+    }
+    return this.items.shift();
+  }
+
+  // Get the front element of the queue without removing it
+  front() {
+    if (this.isEmpty()) {
+      return "Queue is empty";
+    }
+    return this.items[0];
+  }
+
+  // Check if the queue is empty
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  // Get the size of the queue
+  size() {
+    return this.items.length;
+  }
+
+  // Print the elements of the queue
+  printQueue() {
+    console.log(this.items);
+  }
+}
+
+// Usage example
+const queue = new Queue();
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+
+queue.printQueue(); // Output: [1, 2, 3]
+console.log(queue.dequeue()); // Output: 1
+console.log(queue.front()); // Output: 2
+console.log(queue.size()); // Output: 2
+console.log(queue.isEmpty()); // Output: false
 
